de2e80beb82c52d47b3f5f243979906c525a00f5
Aleksandar Oroz <oroz.aleksandar@gmail.com>
2018-06-25 23:25:36 +0200

    cleanup

--- economist/src/com/economist/db/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.economist.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.economist.db.entity.Post;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-	
-}
