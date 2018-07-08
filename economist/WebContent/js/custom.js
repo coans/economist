c9730caf0b20b8f4ca3536da5d516f20aaef0bbb
Aleksandar Oroz <oroz.aleksandar@gmail.com>
2018-07-08 16:32:50 +0200

    JS fix.

--- economist/WebContent/js/custom.js
+++ economist/WebContent/js/custom.js
@@ -2,5 +2,5 @@ function setActiveHeader(url) {
 	$(".nav a[href='"+url+"']").parent().attr("class", "active");
 }
 function makeInputDate() {
-	$(".datepicker").datepicker({ dateFormat: 'dd.mm.yy.', firstDay: 1,dayNamesMin: [ "Ne", "Po", "Ut", "Sr", "&#268;e", "Pe", "Su" ], monthNames: [ "Januar", "Februar", "Mart", "April", "Maj", "Juni", "Juli", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar" ] }).datepicker("setDate", new Date());
+	$(".datepicker").datepicker({ dateFormat: 'dd.mm.yy.', firstDay: 1,dayNamesMin: [ "Ne", "Po", "Ut", "Sr", "&#268;e", "Pe", "Su" ], monthNames: [ "Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar" ] }).datepicker("setDate", new Date());
 }
