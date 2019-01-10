657d2dbb052fa38fd132b1da1e11cb05877b4633
Aleksandar Oroz <oroz.aleksandar@gmail.com>
2019-01-10 18:38:23 +0100

    Repo fix.

--- economist/src/com/economist/db/repository/KomitentRepository.java
+++ economist/src/com/economist/db/repository/KomitentRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.economist.db.entity.Agencija;
 import com.economist.db.entity.Komitent;
-import com.economist.dto.KomitentDTO;
 
 public interface KomitentRepository extends JpaRepository<Komitent, Integer> {
 	
-	public List<KomitentDTO> findByAgencija(Agencija agencija);
+	public List<Komitent> findByAgencija(Agencija agencija);
 }
--- economist/src/com/economist/db/repository/PreduzeceRepository.java
+++ economist/src/com/economist/db/repository/PreduzeceRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.economist.db.entity.Agencija;
 import com.economist.db.entity.Preduzece;
-import com.economist.dto.PreduzeceDTO;
 
 public interface PreduzeceRepository extends JpaRepository<Preduzece, Integer> {
 	
-	List<PreduzeceDTO> findByAgencija(Agencija agencija);
+	List<Preduzece> findByAgencija(Agencija agencija);
 }
